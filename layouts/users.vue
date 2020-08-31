3ba8c3051c9b40664e0709c3890e6ef2651d6de8
Lisa Indraputri <wb-lisaindra532087@antfin.com>
2020-09-01 01:04:54 +0700

    create component ot layout

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Header Users</h1>
+    <Header />
     <Nuxt />
   </div>
 </template>
+<script>
+import Header from '@/components/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
