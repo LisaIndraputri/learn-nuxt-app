a01c6232b45d1a14230dfb75ef983876050512d3
Lisa Indraputri <wb-lisaindra532087@antfin.com>
2020-09-01 22:42:20 +0700

    create about landing page

---
+++ pages/about/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about-wrapper">
+    <h1>About Page</h1>
+    <p>first alinea</p>
+    <p>second alinea</p>
+    <p>third alinea</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .about-wrapper {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
