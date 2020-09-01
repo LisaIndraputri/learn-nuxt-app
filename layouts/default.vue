bf852aa082a1d271b930086d30825492c25eee55
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-01 22:57:23 +0700

    implement header in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    />
     <Nuxt />
   </div>
 </template>
+
+<script>
+  import TheHeader from '@/components/navigation/TheHeader'
+  import TheSidenav from '@/components/navigation/TheSidenav'
+  export default {
+    components: {
+      TheHeader,
+      TheSidenav,
+    }, 
+    data() {
+      return {
+        displaySidenav: false
+      }
+    }
+  }
+</script>
 
 <style>
 html {
