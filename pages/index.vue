fddce24114f80ad4ef3a0754375bfe19e800d21e
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-01 23:05:46 +0700

    generalize component postList

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,15 @@
     <section class="intro">
       <div class="landing-page-title">Get the latest updates!</div>
     </section>
-    <section class="featured-posts">
-      <div class="featured-post-list" v-for="(data, index) in listPreview" :key="index">
-        <postPreview :id="index+1" :data="data"/>
-      </div>
-    </section>
+    <postList />
   </div>
 </template>
 
 <script>
-import postPreview from '@/components/posts/postPreview'
+import postList from '@/components/posts/postList'
 export default {
   components: {
-    postPreview,
-  },
-  computed: {
-    listPreview () {
-      return [{
-          title: 'try with id 1',
-          description: 'this is the desc id 1',
-          url: 'https://i2.wp.com/files.123freevectors.com/wp-content/original/131565-pastel-pink-polygon-abstract-background.jpg?w=800&q=95'
-        }, {
-          title: 'try with id 2',
-          description: 'this is the desc id 2',
-          url: 'https://i2.wp.com/files.123freevectors.com/wp-content/original/131565-pastel-pink-polygon-abstract-background.jpg?w=800&q=95'
-        },
-      ]
-    }
+    postList,
   }
 }
 </script>
@@ -62,18 +44,5 @@ export default {
     font-size: 2rem;
   }
 }
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-.featured-post-list {
-  display: inherit;
-}
-
-
 
 </style>
