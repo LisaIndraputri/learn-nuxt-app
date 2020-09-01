20983c43357695ceca2e1a8f5ea1eb1a5d2d527a
Lisa Indraputri <wb-lisaindra532087@antfin.com>
2020-09-01 22:42:28 +0700

    create posts landing page

---
+++ pages/posts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper-posts">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .wrapper-posts {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
