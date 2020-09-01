69ce1019cb8ae170a607adaa197df796709d7792
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-01 23:56:23 +0700

    create postForm component

---
+++ components/admin/postForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="new-post-form">
+    <form @submit.prevent="onSave">
+      <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+      <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+      <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+      <AppControlInput
+        control-type="textarea"
+        v-model="editedPost.content">
+        Content
+      </AppControlInput>
+      <AppButton type="submit">Save</AppButton>
+      <AppButton
+        type="button"
+        style="margin-left: 10px"
+        btn-style="cancel"
+        @click="onCancel">Cancel
+      </AppButton>
+    </form>
+  </section>
+</template>
+
+<script>
+import AppControlInput from '@/components/uiComp/AppControlInput'
+import AppButton from '@/components/uiComp/AppButton'
+export default {
+  components: {
+    AppButton,
+    AppControlInput,
+  },
+  props: {
+    data: {
+      type: Object,
+      required: false
+    }
+  },
+  data() {
+    return {
+      editedPost: this.data ? {...this.data} : {
+        author: '',
+        title: '',
+        thumbnailLink: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    onSave () {
+      // save wip
+      console.log(this.editedPost, 'save wip')
+    },
+    onCancel () {
+      console.log('cancel wip')
+      this.$router.push('/admin')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
