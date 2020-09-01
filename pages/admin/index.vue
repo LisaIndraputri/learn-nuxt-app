74129e26c0f92f8aec1f10264534ea4576339c0e
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-01 23:27:13 +0700

    create admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>admin page</div>
+  <div class="wrapper-admin">
+    <section class="new-post">
+      <button @click="goToCreatePost">Create a post</button>
+    </section>
+    <section class="existing-post">
+      <h1>Existing posts</h1>
+      <postList />
+    </section>
+  </div>
 </template>
 
 <script>
+import postList from '@/components/posts/postList'
 export default {
-
+  components: {
+    postList,
+  },
+  methods: {
+    goToCreatePost () {
+      this.$router.push('/admin/new-post')
+    }
+  }
 }
 </script>
 
 <style>
-
+.wrapper-admin {
+  padding: 20px;
+}
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+.existing-post {
+  text-align: center;
+}
 </style>
