19aaaae5bb933f88683835ed4a99057b79c5683c
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-02 16:00:48 +0700

    create error layout

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper-error">
+    <h1>Oops, something went wrong!</h1>
+    <p>Back to <a href="/">safety</a>!</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.wrapper-error {
+  text-align: center;
+}
+
+.wrapper-error a {
+  text-decoration: none;
+  color: red;
+}
+
+.wrapper-error a:hover,
+.wrapper-error a:active {
+  color: salmon;
+}
+
+</style>
