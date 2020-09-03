4ad83d651c0165759f4ab03ae7c51fd48ace7a71
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-03 22:38:45 +0700

    adding async data

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="wrapper-single-post">
     <section class="post">
-      <h1 class="post-title">Title of the post</h1>
+      <h1 class="post-title">{{ data.title }}</h1>
       <div class="post-descriptions">
-        <div class="post-description">Last updates on</div>
-        <div class="post-description">written by Name</div>
+        <div class="post-description">Last updates on {{ data.updatedDate }}</div>
+        <div class="post-description">written by{{ data.author }}</div>
       </div>
-      <p>Content of the post</p>
+      <p>{{ data.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Let me know, send a mail to <a href="mailto:lala.gmail.com">lala@gmail.com</a></p>
@@ -16,7 +16,28 @@
 
 <script>
 export default {
-
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            id: '1',
+            title: `try with id ${context.route.params.id}`,
+            description: 'this is the desc id 1',
+            content: 'abcd efgh ijkl',
+            url: 'https://i2.wp.com/files.123freevectors.com/wp-content/original/131565-pastel-pink-polygon-abstract-background.jpg?w=800&q=95',
+            author: 'lala',
+            updatedDate: new Date(),
+  
+          }
+        })
+      }, 1000)
+    })
+      .then(data => {
+        return data
+      })
+      .catch((e) => {context.error(new Error())})
+  },
 }
 </script>
 
