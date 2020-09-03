7d530eddf9cd234080895ed1de6cdb98584ab998
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-04 00:22:05 +0700

    add post with axios

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="wrapper-new-post">
-    <postForm />
+    <postForm @submit="submit"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import postForm from '@/components/admin/postForm'
+import { POST_URL } from '@/constants/db'
 export default {
   layout: 'admin',
   components: {
     postForm,
   },
+  methods: {
+    submit (postData) {
+      this.$store.dispatch('addPost', postData).then(() => {
+        this.$router.push('/admin')
+      })
+    }
+  }
 }
 </script>
 
