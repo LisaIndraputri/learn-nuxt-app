f3792ff2eddd17ed138f8baf50f6bddb0f6b1424
Lisa Indraputri <lisaindraputri98@gmail.com>
2020-09-04 22:59:01 +0700

    create global components

---
+++ plugins/global-components.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import postList from '@/components/posts/postList'
+import InputForm from '@/components/uiComp/Input'
+import SuperButton from '@/components/uiComp/Button'
+
+Vue.component('postList', postList)
+Vue.component('InputForm', InputForm)
+Vue.component('SuperButton', SuperButton)
